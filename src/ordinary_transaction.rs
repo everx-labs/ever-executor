fc407fd6ff48e950917e14ed85ee9f1af6b35493
tonjen <jenkins@tonlabs.io>
2022-12-06 14:57:15 +0000

    Updating the project due to dependency changes: . #none

--- src/ordinary_transaction.rs
+++ src/ordinary_transaction.rs
@@ -279,7 +279,7 @@ impl TransactionExecutor for OrdinaryTransactionExecutor {
                         &compute_phase_gas_fees,
                         actions.unwrap_or_default(),
                         new_data,
-                        &account_address,
+                        account_address,
                         is_special
                     ) {
                         Ok(ActionPhaseResult{phase, messages, copyleft_reward}) => {
@@ -343,7 +343,7 @@ impl TransactionExecutor for OrdinaryTransactionExecutor {
                     &compute_phase_gas_fees, 
                     in_msg, 
                     &mut tr,
-                    &account_address,
+                    account_address,
                     params.block_version,
                 ) {
                     Ok((bounce_ph, Some(bounce_msg))) => {
